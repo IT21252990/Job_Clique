0f9af795b48e86c084201f1bbd154709f6db9aed
KalingaDJ <89300552+IT21252990@users.noreply.github.com>
2023-03-18 16:54:53 +0530

    done Employer Registration Part 2

---
+++ app/src/main/java/com/example/jobclique/EmployerRegistrationNextPart.kt
@@ -0,0 +1,11 @@
+package com.example.jobclique
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmployerRegistrationNextPart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employer_registration_next_part)
+    }
+}
