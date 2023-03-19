2afd5ee40df5f6cccfd9ee09f526a1a72968834e
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2023-03-19 17:57:09 +0530

    complete application detailed part

--- app/src/main/java/com/example/jobclique/MainActivityEmployer.kt
+++ app/src/main/java/com/example/jobclique/MainActivityEmployer.kt
@@ -39,7 +39,7 @@ class MainActivityEmployer : AppCompatActivity() {
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout_employer,fragment)
         fragmentTransaction.commit()
 
     }
