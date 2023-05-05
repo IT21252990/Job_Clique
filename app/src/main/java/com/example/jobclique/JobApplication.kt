e12626cbc54a2309c7631e08beacf9c218b8792a
Vihangi Rathnayake <89300272+IT21271182@users.noreply.github.com>
2023-05-06 00:29:30 +0530

    Application validation and userID

--- app/src/main/java/com/example/jobclique/JobApplication.kt
+++ app/src/main/java/com/example/jobclique/JobApplication.kt
@@ -2,12 +2,13 @@ package com.example.jobclique
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 class JobApplication : AppCompatActivity() {
@@ -17,14 +18,19 @@ class JobApplication : AppCompatActivity() {
     private lateinit var etphone: EditText
     private lateinit var etoptional: EditText
     private lateinit var btnSend: Button
+    private var valid = true
+    private lateinit var userID: String
 
-//  private lateinit var dbRef: DatabaseReference
     private lateinit var dbRef: CollectionReference
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_application)
+
+        userID = FirebaseAuth.getInstance().currentUser!!.uid
 
         etname = findViewById(R.id.edtTextName)
         etemail = findViewById(R.id.editTextEmail)
@@ -32,66 +38,80 @@ class JobApplication : AppCompatActivity() {
         etoptional = findViewById(R.id.edtTextOptional)
         btnSend = findViewById(R.id.btnApplicationSend)
 
-//        dbRef = FirebaseDatabase.getInstance().getReference("Applications")
         dbRef = FirebaseFirestore.getInstance().collection("JobApplications")
 
         btnSend.setOnClickListener{
-            sendApplication()
+
+            checkField(etname)
+            checkField(etemail)
+            checkField(etphone)
+
+            //getting values
+            val name = etname.text.toString()
+            val email = etemail.text.toString()
+            val phone = etphone.text.toString()
+            val optional = etoptional.text.toString()
+            val status = "Pending"
+            val appliedDate = FieldValue.serverTimestamp()
+//            val user = userID
+
+            if ( name.isEmpty() ){
+                etname.error = "Please enter the name !"
+                etname.requestFocus()
+                return@setOnClickListener
+
+            }
+            if ( email.isEmpty() ){
+                etemail.error = "Email required !"
+                etemail.requestFocus()
+                return@setOnClickListener
+            }
+            if( !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                etemail.error = ("Please Enter valid Email !")
+                etemail.requestFocus()
+                return@setOnClickListener
+            }
+            if ( phone.isEmpty() ){
+                etphone.error = "Phone number required !"
+                etphone.requestFocus()
+                return@setOnClickListener
+            }
+            if( phone.length != 10){
+                etphone.error = ("Phone number should have 10 digits !")
+                etphone.requestFocus()
+                return@setOnClickListener
+            }
+
+            val application = JobApplicationModel(name, email, phone,optional,status,appliedDate,userID)
+
+            if (valid) {
+                dbRef.add(application)
+                    .addOnSuccessListener { documentReference ->
+                        Toast.makeText(this, "Application sent successfully!", Toast.LENGTH_LONG).show()
+
+                        etname.text.clear()
+                        etemail.text.clear()
+                        etphone.text.clear()
+                        etoptional.text.clear()
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(this, "Error ${exception.message}", Toast.LENGTH_LONG).show()
+                    }
+            }
         }
 
     }
 
-    private fun sendApplication(){
 
-        //getting values
-        val name = etname.text.toString()
-        val email = etemail.text.toString()
-        val phone = etphone.text.toString()
-        val optional = etoptional.text.toString()
-
-        if ( name.isEmpty() ){
-            etname.error = "Please enter the name !"
-        }
-        if ( email.isEmpty() ){
-            etemail.error = "Email required !"
-        }
-        if ( phone.isEmpty() ){
-            etphone.error = "Phone number required !"
+    private fun checkField(textField: EditText): Boolean {
+        if (textField.text.toString().isEmpty()) {
+            textField.error = "Error"
+            valid = false
+        } else {
+            valid = true
         }
 
-//        val applicationId = dbRef.push().key!!
-//
-//        val application = JobApplicationModel(applicationId,name,email,phone)
-//
-//        dbRef.child(applicationId).setValue(application)
-//            .addOnCompleteListener{
-//                Toast.makeText(this,"Application sent successfully !", Toast.LENGTH_LONG).show()
-//
-//                etname.text.clear()
-//                etemail.text.clear()
-//                etphone.text.clear()
-//                etoptional.text.clear()
-//
-//            }
-//            .addOnFailureListener{ err ->
-//                Toast.makeText(this,"Error ${err.message}", Toast.LENGTH_LONG).show()
-//            }
-
-        val application = JobApplicationModel(name, email, phone,optional)
-
-        dbRef.add(application)
-            .addOnSuccessListener { documentReference ->
-                Toast.makeText(this, "Application sent successfully!", Toast.LENGTH_LONG).show()
-
-                etname.text.clear()
-                etemail.text.clear()
-                etphone.text.clear()
-                etoptional.text.clear()
-            }
-            .addOnFailureListener { exception ->
-                Toast.makeText(this, "Error ${exception.message}", Toast.LENGTH_LONG).show()
-            }
-
-
+        return valid
     }
+
 }
--- app/src/main/java/com/example/jobclique/JobApplicationModel.kt
+++ app/src/main/java/com/example/jobclique/JobApplicationModel.kt
@@ -1,13 +1,15 @@
 package com.example.jobclique
 
-class JobApplicationModel (
+import com.google.firebase.firestore.FieldValue
+
+class JobApplicationModel(
     var Name: String? = null,
     var Email: String? = null,
     var PhoneNo: String? = null,
     var Optional: String? = null,
-//    var AppliedDate: String? = null,
-//    var Status: String? = null,
-//    var UserID: String? = null,
+    var Status: String? = null,
+    var AppliedDate: FieldValue ?= null,
+    var UserID: String? = null,
 //    var JobPostID: String? = null,
 
 
